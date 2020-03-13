fb988878fa1f834da1937a93635e1e7fd8ce40ee
John Kang <pollyfactor@hotmail.com>
2020-03-13 19:45:39 -0400

    updated textarea

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   //columns for hour, events, and save buttons
     var colHour = $(`<div class="col-sm-2 hour"><h4>` + amPm(i) + `</h4>`);
     var colEvent = $(
-      `<div class="col-sm-8 texarea"><textarea placeholder="Add an event"></textarea>`
+      `<div class="col-sm-8"><textarea placeholder="Add an event"></textarea>`
     );
     var colSave= $('<div class="col-sm-2 saveBtn"><button><i class="far fa-save"></i></button>');
 
