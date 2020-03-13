b88a33be536c646fcc88b275176f1b3aab5b0f83
John Kang <pollyfactor@hotmail.com>
2020-03-13 19:33:54 -0400

    updated correct hours

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
 
   
   //creating grid for calendar
-  for (var i = 0; i < 12; i++) {
+  for (var i = 9; i < 18; i++) {
     var rowEl = $(`<div data-row=${i} id='${i}' class="row">`);
 
+   
   //columns for hour, events, and save buttons
-    var colHour = $(`<div class="col-sm-2 hour"><h4>${i}</h4>`);
+    var colHour = $(`<div class="col-sm-2 hour"><h4>` + amPm(i) + `</h4>`);
     var colEvent = $(
-      `<div class="col-lg-8 texarea"><textarea placeholder="Add an event"></textarea>`
+      `<div class="col-sm-8 texarea"><textarea placeholder="Add an event"></textarea>`
     );
     var colSave= $('<div class="col-sm-2 saveBtn"><button><i class="far fa-save"></i></button>');
 
@@ -22,5 +23,18 @@ $(document).ready(function() {
 
   //append to container
     $(".container").append(rowEl);
+
+    //determines am or pm
+    function amPm(x) {
+        if (x < 12){
+            return(x + " AM");
+        } else if (x === 12) {
+            return(x + " PM");
+        } else {
+            return((x - 12) + " PM");
+        }
+      }
   } 
+
+  
 });
