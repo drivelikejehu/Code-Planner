343da77ac815692bb55089868425aa3c5810eb9f
John Kang <pollyfactor@hotmail.com>
2020-03-13 20:12:18 -0400

    updated script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
 //current day
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
   
   //creating grid for calendar
   for (var i = 9; i < 18; i++) {
     var rowEl = $(`<div data-row=${i} id='${i}' class="row">`);
 
-   
   //columns for hour, events, and save buttons
     var colHour = $(`<div class="col-sm-2 hour"><h4>` + amPm(i) + `</h4>`);
     var colEvent = $(
-      `<div class="col-sm-8"><textarea placeholder="Add an event"></textarea>`
+      `<div class="col-sm-8 event"><textarea placeholder="Add an event"></textarea>`
     );
     var colSave= $('<div class="col-sm-2 saveBtn"><button><i class="far fa-save"></i></button>');
 
